50b5543197a9237dc1795972995d7b86a79b76c2
NDRue <lostkyo@gmail.com>
2012-11-23 18:04:51 +0800

    23/11: Final push to Open Beta

--- gen/com/ndrue/gathereroffline/R.java
+++ gen/com/ndrue/gathereroffline/R.java
@@ -8,12 +8,16 @@
 package com.ndrue.gathereroffline;
 
 public final class R {
+    public static final class anim {
+        public static final int fadein=0x7f040000;
+        public static final int fadeout=0x7f040001;
+    }
     public static final class attr {
     }
     public static final class dimen {
-        public static final int padding_large=0x7f050002;
-        public static final int padding_medium=0x7f050001;
-        public static final int padding_small=0x7f050000;
+        public static final int padding_large=0x7f060002;
+        public static final int padding_medium=0x7f060001;
+        public static final int padding_small=0x7f060000;
     }
     public static final class drawable {
         public static final int blackrounded=0x7f020000;
@@ -26,35 +30,38 @@ public final class R {
         public static final int penalties_guide=0x7f020007;
         public static final int resolvespells_copiable=0x7f020008;
         public static final int reviewfeedback=0x7f020009;
+        public static final int standard_border=0x7f02000a;
     }
     public static final class id {
-        public static final int addfourbutton=0x7f090010;
-        public static final int addonebutton=0x7f09000d;
-        public static final int addthreebutton=0x7f09000f;
-        public static final int addtwobutton=0x7f09000e;
-        public static final int backbutton1=0x7f090006;
-        public static final int backbutton2=0x7f09000a;
-        public static final int cardImage=0x7f090007;
-        public static final int carddetails=0x7f090009;
-        public static final int comprmainview=0x7f09000b;
-        public static final int deckCount=0x7f09000c;
-        public static final int downloadImage=0x7f090008;
-        public static final int editText1=0x7f090000;
-        public static final int homescreenoptions=0x7f090015;
-        public static final int imageLayout=0x7f090016;
-        public static final int imageView1=0x7f090013;
-        public static final int infoprogressbar=0x7f090002;
-        public static final int infotextview=0x7f090003;
-        public static final int infoviewbar=0x7f090001;
-        public static final int listView1=0x7f090004;
-        public static final int menu_settings=0x7f09001a;
-        public static final int name_entry=0x7f090018;
-        public static final int resetbutton=0x7f090011;
-        public static final int scrollView1=0x7f090005;
-        public static final int textData=0x7f090019;
-        public static final int textView1=0x7f090014;
-        public static final int undobutton=0x7f090012;
-        public static final int zoomButton1=0x7f090017;
+        public static final int addfourbutton=0x7f0a0010;
+        public static final int addonebutton=0x7f0a000d;
+        public static final int addthreebutton=0x7f0a000f;
+        public static final int addtwobutton=0x7f0a000e;
+        public static final int backbutton1=0x7f0a0006;
+        public static final int backbutton2=0x7f0a000a;
+        public static final int cardImage=0x7f0a0007;
+        public static final int carddetails=0x7f0a0009;
+        public static final int comprmainview=0x7f0a000b;
+        public static final int deckCount=0x7f0a000c;
+        public static final int downloadImage=0x7f0a0008;
+        public static final int editText1=0x7f0a0000;
+        public static final int homescreenoptions=0x7f0a0015;
+        public static final int homescreenoptions2=0x7f0a0017;
+        public static final int imageLayout=0x7f0a0018;
+        public static final int imageView1=0x7f0a0013;
+        public static final int infoprogressbar=0x7f0a0002;
+        public static final int infotextview=0x7f0a0003;
+        public static final int infoviewbar=0x7f0a0001;
+        public static final int listView1=0x7f0a0004;
+        public static final int menu_settings=0x7f0a001c;
+        public static final int name_entry=0x7f0a001a;
+        public static final int resetbutton=0x7f0a0011;
+        public static final int scrollView1=0x7f0a0005;
+        public static final int submenulayout=0x7f0a0016;
+        public static final int textData=0x7f0a001b;
+        public static final int textView1=0x7f0a0014;
+        public static final int undobutton=0x7f0a0012;
+        public static final int zoomButton1=0x7f0a0019;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -69,21 +76,21 @@ public final class R {
         public static final int text_only=0x7f030009;
     }
     public static final class menu {
-        public static final int activity_main=0x7f080000;
+        public static final int activity_main=0x7f090000;
     }
     public static final class string {
-        public static final int app_name=0x7f060000;
-        public static final int hello_world=0x7f060001;
-        public static final int homescreentext=0x7f060005;
-        public static final int imagedescriptor=0x7f060006;
-        public static final int menu_settings=0x7f060002;
-        public static final int search_hint=0x7f060004;
-        public static final int title_activity_main=0x7f060003;
+        public static final int app_name=0x7f070000;
+        public static final int hello_world=0x7f070001;
+        public static final int homescreentext=0x7f070005;
+        public static final int imagedescriptor=0x7f070006;
+        public static final int menu_settings=0x7f070002;
+        public static final int search_hint=0x7f070004;
+        public static final int title_activity_main=0x7f070003;
     }
     public static final class style {
-        public static final int AppTheme=0x7f070000;
+        public static final int AppTheme=0x7f080000;
     }
     public static final class xml {
-        public static final int compr_rules=0x7f040000;
+        public static final int compr_rules=0x7f050000;
     }
 }
--- src/com/ndrue/gathereroffline/HomeScreen.java
+++ src/com/ndrue/gathereroffline/HomeScreen.java
@@ -6,10 +6,16 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -17,6 +23,11 @@ public class HomeScreen extends Activity {
 
 	private ArrayList<String> hsItems;
 	private ArrayAdapter<String> hsAdapter;
+	private ArrayList<String> hsItemsSub;
+	private ArrayAdapter<String> hsAdapterSub;
+	private final String[] hsItemsHeader = new String[] { "Quick reference >",
+			"Rules >", "Misc >" };
+	private String[][] hsItemsArray = new String[3][10];
 	private final String[] hsItemsInArray = new String[] { "Penalties",
 			"Layers / Casting Spells",
 			"Resolving Spells / Copiable Characteristics",
@@ -26,6 +37,9 @@ public class HomeScreen extends Activity {
 			"Magic Tournament Rules", "Add On", "Miscellaneous Options" };
 	private Context ct;
 	private LogCatcher logC;
+	private int hsLevel = 0;
+	private int hsParent = -1;
+	private final String pid = "HomeScreen";
 
 	@Override
 	public void onCreate(Bundle b) {
@@ -37,8 +51,22 @@ public class HomeScreen extends Activity {
 			hsItems = new ArrayList<String>();
 			hsAdapter = new ArrayAdapter<String>(this, R.layout.listview_item,
 					hsItems);
-			for (int i = 0; i < hsItemsInArray.length; ++i) {
-				hsItems.add(hsItemsInArray[i]);
+			hsItemsSub = new ArrayList<String>();
+			hsAdapterSub = new ArrayAdapter<String>(this,
+					R.layout.listview_item, hsItemsSub);
+			hsItemsArray[0] = new String[] { "> Penalties",
+					"> Layers / Casting Spells",
+					"> Resolving Spells / Copiable Characteristics",
+					"> Types of Information", "> Head Judge Announcement",
+					"> Reviews / Feedback" };
+			hsItemsArray[1] = new String[] { "> Comprehensive Rules",
+					"> Infraction Procedure Guide", "> Magic Tournament Rules" };
+			hsItemsArray[2] = new String[] { "> Oracle", "> Decklist Counter",
+					"> Add On", "> Miscellaneous Options" };
+			// for (int i = 0; i < hsItemsInArray.length; ++i) {
+			for (int i = 0; i < hsItemsHeader.length; ++i) {
+				// hsItems.add(hsItemsInArray[i]);
+				hsItems.add(hsItemsHeader[i]);
 			}
 			ListView lv = (ListView) findViewById(R.id.homescreenoptions);
 			lv.setAdapter(hsAdapter);
@@ -52,6 +80,18 @@ public class HomeScreen extends Activity {
 
 			});
 			hsAdapter.notifyDataSetChanged();
+			ListView lv2 = (ListView) findViewById(R.id.homescreenoptions2);
+			lv2.setAdapter(hsAdapterSub);
+			lv2.setOnItemClickListener(new OnItemClickListener() {
+
+				public void onItemClick(AdapterView<?> arg0, View arg1,
+						int arg2, long arg3) {
+					// TODO Auto-generated method stub
+					navigate2(((TextView) arg1).getText().toString());
+				}
+
+			});
+
 			Intent itnt = new Intent(this, InitCardData.class);
 			startActivityForResult(itnt, 1);
 		} catch (Exception e) {
@@ -62,77 +102,164 @@ public class HomeScreen extends Activity {
 	private void navigate(String dir) {
 		try {
 			int sel = -1;
-			Intent reUsable;
-			Bundle reuseBundle;
-			for (int i = 0; i < hsItemsInArray.length; ++i) {
-				if (hsItemsInArray[i].equals(dir)) {
-					sel = i;
+			if (hsLevel == 0) {
+				for (int i = 0; i < hsItemsHeader.length; ++i) {
+					if (hsItemsHeader[i].equals(dir)) {
+						sel = i;
+					}
 				}
-			}
-			switch (sel) {
-			case 0:
-				reUsable = new Intent(ct, ImageOnly.class);
-				reUsable.putExtra("image", R.drawable.penalties_guide);
-				startActivity(reUsable);
-				break;
-			case 1:
-				reUsable = new Intent(ct, ImageOnly.class);
-				reUsable.putExtra("image", R.drawable.layers_castingspells);
-				startActivity(reUsable);
-				break;
-			case 2:
-				reUsable = new Intent(ct, ImageOnly.class);
-				reUsable.putExtra("image", R.drawable.resolvespells_copiable);
-				startActivity(reUsable);
-				break;
-			case 3:
-				reUsable = new Intent(ct, ImageOnly.class);
-				reUsable.putExtra("image", R.drawable.information_details);
-				startActivity(reUsable);
-				break;
-			case 4:
-				reUsable = new Intent(ct, ImageOnly.class);
-				reUsable.putExtra("image", R.drawable.hjannouncement);
-				startActivity(reUsable);
-				break;
-			case 5:
-				reUsable = new Intent(ct, ImageOnly.class);
-				reUsable.putExtra("image", R.drawable.reviewfeedback);
-				startActivity(reUsable);
-				break;
-			case 6:
-				reUsable = new Intent(ct, MainActivity.class);
-				startActivity(reUsable);
-				break;
-			case 7:
-				reUsable = new Intent(ct, DecklistCounter.class);
-				startActivity(reUsable);
-				break;
-			case 8:
-				reUsable = new Intent(ct, ComprehensiveRules.class);
-				startActivity(reUsable);
-				break;
-			case 9:
-				reUsable = new Intent(ct, InfractionPenalty.class);
-				startActivity(reUsable);
-				break;
-			case 10:
-				reUsable = new Intent(ct, TournamentRules.class);
-				startActivity(reUsable);
-				break;
-			case 11:
-				reUsable = new Intent(ct, ExtraAddOns.class);
-				startActivity(reUsable);
-				break;
-			case 12:
-				reUsable = new Intent(ct, MiscOptions.class);
-				startActivity(reUsable);
-				break;
-			default:
-				break;
+				hsItemsSub.clear();
+				for (int i = 0; i < hsItemsArray[sel].length; ++i) {
+					hsItemsSub.add(hsItemsArray[sel][i]);
+				}
+				hsParent = sel;
+				LinearLayout ll = (LinearLayout)findViewById(R.id.submenulayout);
+				ll.setVisibility(View.VISIBLE);
+				hsAdapterSub.notifyDataSetChanged();
+				++hsLevel;
+				Animation animationFadeIn = AnimationUtils.loadAnimation(this, R.anim.fadein);
+			       ll.startAnimation(animationFadeIn);				
 			}
 		} catch (Exception e) {
 			logC.write(e.getMessage());
 		}
+	}
+
+	private void navigate2(String dir) {
+		try {
+			int sel = -1;
+			if (hsLevel == 1) {
+				Intent reUsable;
+				// Bundle reuseBundle;
+				for (int i = 0; i < hsItemsArray[hsParent].length; ++i) {
+					if (hsItemsArray[hsParent][i].equals(dir)) {
+						sel = i;
+					}
+				}
+				switch (hsParent) {
+				case 0:
+					switch (sel) {
+					case 0:
+						reUsable = new Intent(ct, ImageOnly.class);
+						reUsable.putExtra("image", R.drawable.penalties_guide);
+						startActivity(reUsable);
+						break;
+					case 1:
+						reUsable = new Intent(ct, ImageOnly.class);
+						reUsable.putExtra("image",
+								R.drawable.layers_castingspells);
+						startActivity(reUsable);
+						break;
+					case 2:
+						reUsable = new Intent(ct, ImageOnly.class);
+						reUsable.putExtra("image",
+								R.drawable.resolvespells_copiable);
+						startActivity(reUsable);
+						break;
+					case 3:
+						reUsable = new Intent(ct, ImageOnly.class);
+						reUsable.putExtra("image",
+								R.drawable.information_details);
+						startActivity(reUsable);
+						break;
+					case 4:
+						reUsable = new Intent(ct, ImageOnly.class);
+						reUsable.putExtra("image", R.drawable.hjannouncement);
+						startActivity(reUsable);
+						break;
+					case 5:
+						reUsable = new Intent(ct, ImageOnly.class);
+						reUsable.putExtra("image", R.drawable.reviewfeedback);
+						startActivity(reUsable);
+						break;
+					}
+					break;
+				case 1:
+					switch (sel) {
+					case 0:
+						reUsable = new Intent(ct, ComprehensiveRules.class);
+						startActivity(reUsable);
+						break;
+					case 1:
+						reUsable = new Intent(ct, InfractionPenalty.class);
+						startActivity(reUsable);
+						break;
+					case 2:
+						reUsable = new Intent(ct, TournamentRules.class);
+						startActivity(reUsable);
+						break;
+					}
+					break;
+				case 2:
+					switch (sel) {
+					case 0:
+						reUsable = new Intent(ct, MainActivity.class);
+						startActivity(reUsable);
+						break;
+					case 1:
+						reUsable = new Intent(ct, DecklistCounter.class);
+						startActivity(reUsable);
+						break;
+					case 2:
+						reUsable = new Intent(ct, ExtraAddOns.class);
+						startActivity(reUsable);
+						break;
+					case 3:
+						reUsable = new Intent(ct, MiscOptions.class);
+						startActivity(reUsable);
+						break;
+					}
+					break;
+				default:
+					break;
+				}
+			}
+		} catch (Exception e) {
+			logC.write(e.getMessage());
+		}
+	}
+
+	@Override
+	public void onBackPressed() {
+		if (hsLevel == 0) {
+			finish();
+		} else {
+			hsItemsSub.clear();
+			hsLevel = 0;
+			hsParent = -1;
+			LinearLayout ll = (LinearLayout)findViewById(R.id.submenulayout);
+			Animation animationFadeOut = AnimationUtils.loadAnimation(this, R.anim.fadeout);
+		       ll.startAnimation(animationFadeOut);
+		     animationFadeOut.setAnimationListener(new AnimationListener() {
+
+				public void onAnimationEnd(Animation arg0) {
+					// TODO Auto-generated method stub
+					((LinearLayout) findViewById(R.id.submenulayout))
+					.setVisibility(View.GONE);
+				}
+
+				public void onAnimationRepeat(Animation animation) {
+					// TODO Auto-generated method stub
+					
+				}
+
+				public void onAnimationStart(Animation animation) {
+					// TODO Auto-generated method stub
+					
+				}
+		    	 
+		     });
+		}
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (Integer.parseInt(android.os.Build.VERSION.SDK) < 5
+				&& keyCode == KeyEvent.KEYCODE_BACK
+				&& event.getRepeatCount() == 0) {
+			onBackPressed();
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
 	}
 }
